cace5a6decba29825bc3dee16095fb6a909538c2
Xavier Riley <xavier@kyanmedia.com>
2013-03-08 20:05:30 +0000

    Hide data structures

---
+++ revealing_references.rb
@@ -0,0 +1,20 @@
+class RevealingReferences
+  attr_reader :wheels
+  def initialize(data)
+    @wheels = wheelify(data)
+  end
+
+  def diameters
+    wheels.collect {|wheel|
+      wheel.rim + (wheel.tire * 2)}
+  end
+
+  Wheel = Struct.new(:rim, :tire)
+  def wheelify(data)
+    data.collect {|cell|
+      Wheel.new(cell[0], cell[1])}
+  end
+end
+
+# The diameters method no longer has knowledge of the internal structure of the data array
+# What were once references to cell[1] have been transformed into message sends to wheel.tire
