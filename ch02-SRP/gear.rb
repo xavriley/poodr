8752a3cf8fc04d89afeeaf3591fe0d698c72f449
Xavier Riley <xavier@kyanmedia.com>
2013-03-08 19:54:44 +0000

    depend on behaviour, not data

--- ch02-SRP/gear.rb
+++ ch02-SRP/gear.rb
@@ -1,23 +1,17 @@
 class Gear
-  attr_reader :chainring, :cog, :rim, :tire
-  def initialize(chainring, cog, rim, tire)
+  attr_reader :chainring, :cog
+  def initialize(chainring, cog)
     @chainring = chainring
     @cog = cog
-    @rim = rim
-    @tire = tire
   end
 
   def ratio
+    #@chainring / @cog.to_f <------ road to ruin
     chainring / cog.to_f
-  end
-
-  def gear_inches
-    # tire goes around rim twice for diameter
-    ratio * (rim + (tire *2))
   end
 end
 
-#This change works but breaks the previous interface
-
-#puts Gear.new(52, 11i, 26, 1.5).ratio
-#puts Gear.new(52, 11, 24, 1.25).ratio
+# Write code that embraces change
+# - Depend on behaviour, not data
+#     Wrap instance variables in methods to DRY them up.
+#     Their behaviour can be changed in one place
